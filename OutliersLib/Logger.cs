cd7643e9278ed2b2f5f9ea4c1e545bf4e9a7e0fd
vladislav <vlad.dineev@gmail.com>
2020-05-19 10:43:59 +0300

    Final commit

---
+++ OutliersLib/Logger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace OutliersLib
+{
+    public static class Logger
+    {
+        public static StreamWriter fs = File.AppendText("Log.txt");
+        public static void Push(string message)
+        {
+            fs.WriteLine(DateTime.Now.ToString("hh.mm.ss.ffffff") + ": >" + message);
+            fs.Flush();
+        }
+    }
+}
